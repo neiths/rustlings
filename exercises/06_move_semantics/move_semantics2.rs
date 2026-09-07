454b47112c21c1b69181bc96dcc2c925299aef3b
neiths <thienkute01022002@gmail.com>
2026-09-07 23:54:55 +0700

    complete ex 6

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -20,7 +20,9 @@ mod tests {
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let temp = vec0.clone();
+
+        let vec1 = fill_vec(temp);
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
